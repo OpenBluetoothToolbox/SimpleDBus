214a68043c37def9caf7a9cabf6b483487ce6e5a
Kevin Dewald <kdewald@itba.edu.ar>
2021-12-13 22:39:31 -0800

    Fixed examples

--- include/simpledbus/SimpleDBus.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "base/Connection.h"
-#include "base/Message.h"
-
-#include "interfaces/Introspectable.h"
-#include "interfaces/ObjectManager.h"
-#include "interfaces/Properties.h"
-#include "interfaces/PropertyHandler.h"
